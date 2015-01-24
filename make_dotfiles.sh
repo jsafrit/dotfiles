f6331db9a410c7560f0f5d06ca1efccade457bc0
John Safrit <jsafrit@users.noreply.github.com>
2015-01-24 08:30:09 -0500

    Removed non existing example dotfiles from versioning list

--- make_dotfiles.sh
+++ make_dotfiles.sh
@@ -8,7 +8,7 @@
 
 dir=~/.dotfiles                    # dotfiles directory
 #olddir=~/.dotfiles_old             # old dotfiles backup directory
-files="bashrc bash_aliases bash_logout gitconfig gitignore vimrc vim"  # list of files/folders to symlink in homedir  
+files="bashrc bash_aliases bash_logout gitconfig gitignore"  # list of files/folders to symlink in homedir  
 
 ##########
 #cd ~
